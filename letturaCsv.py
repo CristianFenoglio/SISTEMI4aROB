377e9aacfab51fc4a2f1f2d129715a75e50d3af6
FenoglioC <55739443+CristianFenoglio@users.noreply.github.com>
2020-12-15 11:37:41 +0100

    Add files via upload

--- letturaCsv.py
+++ letturaCsv.py
@@ -1,21 +1,30 @@
+import random
 def playrandom(playlist):
-    for i,elemento 
+    random.seed()
+    lisControl=[]
+    while len(lisControl)!=len(playlist):
+        num=random.randrange(len(playlist))
+        if(not(num in lisControl)):
+            print(playlist[num])
+            lisControl.append(num)
 
 
-file=open("./music.csv","r")
-playlist=[]
-for riga in file:
-    #print(riga, end=" ")#l'end permette di modificare la messa a capo automatica
-                        #evita infatti che vad<a a capo in automatico e puoi mettere un carattere qualsiasi
-    a=riga[0:-1].split(",")
-    appoggio={"numero":a[0], "nome":a[1], "author":a[2]}
-    playlist.append(appoggio)
+        
 
-    
-print(playlist)
+def main():
+    file=open("./music.csv","r")
+    playlist=[]
+    for riga in file:
+        #print(riga, end=" ")#l'end permette di modificare la messa a capo automatica
+                            #evita infatti che vada a capo in automatico e puoi mettere un carattere qualsiasi
+        pezzoR=riga[0:-1].split(",")
+        appoggio={"numero":pezzoR[0], "nome":pezzoR[1], "author":pezzoR[2]}
+        playlist.append(appoggio)
+    #print(playlist)
+    playrandom(playlist)
+    #playran4  dom(playlist)
+    file.close()
 
-playrandom(playlist)
 
-
-
-file.close()
+if __name__ == "__main__":
+	main()
